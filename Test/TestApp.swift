e706ce7e455a9e7b4b99fc964855a78236e6e94e
Hijikinoheya <hisaki714@yahoo.co.jp>
2024-12-01 11:48:07 +0800

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by HisakiKoike on 2024/12/01.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
